a7f84d2344b18c892382de32e3e10b0e97a1be43
mlsnoeij <89518034+mlsnoeij@users.noreply.github.com>
2021-11-05 15:55:25 +0100

    Add files via upload

---
+++ JS Basics/index.js
@@ -0,0 +1,22 @@
+//This is my first Javascript code!
+console.log('Hello Winc Academy');
+
+let firstName = 'Marieke'
+
+console.log(firstName)
+
+let number1 = 5
+let number2 = 10
+
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 / number2)
+console.log(number1 * number2)
+console.log(number1 % number2)
+
+let age = 39
+
+console.log(typeof age)
+
+age = '39'
+console.log(typeof age)
