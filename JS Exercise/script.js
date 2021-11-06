ee9341854d1475c6b77f5857564c7e77d10df460
mlsnoeij <89518034+mlsnoeij@users.noreply.github.com>
2021-11-06 13:39:19 +0100

    Add files via upload

---
+++ JS Exercise/script.js
@@ -0,0 +1,26 @@
+const age = 39;
+
+if (age > 18) {
+    console.log("you are allowed in!")
+}
+else {
+    console.log("I'm sorry, you are too young")
+};
+
+const isFemale = true;
+
+if (isFemale){
+    console.log("Woman")
+}
+else{
+    console.log("Man")
+}; 
+
+const driverStatus = "bob"
+
+if(driverStatus=="bob"){
+    console.log("You are allowed to drive")
+}
+else{
+    console.log("You are too drunk to drive")
+}
