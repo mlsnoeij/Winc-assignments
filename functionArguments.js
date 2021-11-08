0c036b5db30033a9f5a22046edc5765fd82c8573
mlsnoeij <89518034+mlsnoeij@users.noreply.github.com>
2021-11-08 10:12:23 +0100

    Add files via upload

---
+++ functionArguments.js
@@ -0,0 +1,22 @@
+// const paintWalls = function(){
+//     console.log("The wall has been painted red")
+// };
+
+// paintWalls();
+
+// const paintWalls = function(color){
+//     console.log("The wall has been painted "+ color)
+// };
+
+// paintWalls("red");
+// paintWalls("green");
+// paintWalls();
+
+
+const paintWalls = function(wall, color){
+    console.log("The "+wall+ " wall has been painted "+ color)
+};
+
+paintWalls("north", "red");
+paintWalls("east", "green");
+
