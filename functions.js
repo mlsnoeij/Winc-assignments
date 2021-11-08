24f1f0d2e9f9cc291f1ec990b59c241c9048fcee
mlsnoeij <89518034+mlsnoeij@users.noreply.github.com>
2021-11-08 09:18:40 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,19 @@
+const makePizza = function(){
+    console.log("make dough")
+    console.log("add toppings")
+    console.log("put in the oven")
+}
+
+const makeSushi = function(){
+    console.log("boil rice")
+    console.log("add rice and toppings to norisheet")
+    console.log("roll em up!")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
+
