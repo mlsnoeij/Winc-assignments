18639f7e1f2740f02dc46e0bf007dd71f909a50a
mlsnoeij <89518034+mlsnoeij@users.noreply.github.com>
2021-11-08 16:19:23 +0100

    Add files via upload

---
+++ function3ways.js
@@ -0,0 +1,21 @@
+function Mathproblem (number1, number2){
+    const calculation =((number1*number1)+(number2*number2))*((number1*number1)+(number2*number2))
+   return calculation
+}
+
+console.log(Mathproblem(2,2));
+
+const Mathproblem = function(number1,number2){
+    const calculation =((number1*number1)+(number2*number2))*((number1*number1)+(number2*number2))
+    return calculation
+}
+
+console.log(Mathproblem(2,2))
+
+
+const Mathproblem = (number1, number2)=> {
+     const calculation =((number1*number1)+(number2*number2))*((number1*number1)+(number2*number2))
+    return calculation
+}
+
+console.log(Mathproblem(2,2))
